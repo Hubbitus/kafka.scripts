9abfbc8c1159eacc924e3b0e0a9068f3e26b7b4d
Pavel Alexeev <pahan@hubbitus.info>
2020-08-20 19:17:54 +0300

    Add script _kafka.GetOffsetShell.sh

---
+++ _kafka.GetOffsetShell.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+source "$(dirname $0)/.config.sh"
+
+# By https://stackoverflow.com/questions/28579948/java-how-to-get-number-of-messages-in-a-topic-in-apache-kafka/50083376#50083376
+
+: ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
+: ${TOPIC?"Not enough vars set: TOPIC required. Script to count amount of messages in topic.  Example: TOPIC=topic1 $0"}
+
+docker exec -it $(kafka_exec_cache) kafka-run-class kafka.tools.GetOffsetShell --broker-list ${KAFKA_BOOTSTRAP_SERVERS} \
+	--topic $TOPIC \
+		"$@" 2>&1
--- _kafka.consume-topic.sh
+++ _kafka.consume-topic.sh
@@ -4,7 +4,7 @@ source "$(dirname $0)/.config.sh"
 
 
 : ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
-: ${TOPIC?"Not enough vars set: TOPIC required"}
+: ${TOPIC?"Not enough vars set: TOPIC required. Example: TOPIC=topic1 $0"}
 
 
 docker exec -it $(kafka_exec_cache) kafka-console-consumer --bootstrap-server $KAFKA_BOOTSTRAP_SERVERS \
