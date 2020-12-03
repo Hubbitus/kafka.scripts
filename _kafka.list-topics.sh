7621858e297d23237643fa5ed7ca05ef913f00f9
Pavel Alexeev <pahan@hubbitus.info>
2020-12-03 12:42:01 +0300

    Add _kafka.list-topics.sh

---
+++ _kafka.list-topics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to list present kafka topics.
+# Optionally filtered by KAFKA_TOPICS_FILTER (PERL regexp supported).
+# F.e.:
+# KAFKA_TOPICS_FILTER=scoring ./_kafka.list-topics.sh
+
+source "$(dirname $0)/.config.sh"
+
+: ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
+
+docker exec -it $(kafka_exec_cache) kafka-topics --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" \
+	--list 2>&1 \
+		| grep -P ${KAFKA_TOPICS_FILTER-.} \
