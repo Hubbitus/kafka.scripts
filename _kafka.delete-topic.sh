9dbf7408bf67d5256d4b767c77e198a6f060ba40
Pavel Alexeev <pahan@hubbitus.info>
2020-12-09 01:40:28 +0300

    Add _kafka.delete-topic.sh

---
+++ _kafka.delete-topic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to delete specified in TOPIC variable Kafka topic
+
+: ${TOPIC?"Not enough vars set: TOPIC required. Because it destructive operation we do NOT read topic name from config.sh file! Please provide it directly. F.e.: 'TOPIC=uat-scoring.match-data.Scoring ./$(basename $0)'"}
+
+source "$(dirname $0)/.config.sh"
+
+: ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
+
+docker exec -it $(kafka_exec_cache) kafka-topics --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" \
+		--delete --topic "${TOPIC}" 2>&1
