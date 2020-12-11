c2989189487f90ac78636bfb88fd7a2c8febc71f
Pavel Alexeev <pahan@hubbitus.info>
2020-12-11 14:34:30 +0300

    Add _kafka.delete-topics.sh, fix logging

---
+++ _kafka.delete-topics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Delete multiple kafka topics.
+# For filtering you may provide optional variable KAFKA_TOPICS_FILTER
+# See ./_kafka.list-topics.sh script description and examples
+source $(dirname $0)/.config.sh
+
+i=0
+
+for TOPIC in $($(dirname $0)/_kafka.list-topics.sh); do
+	echo "Deleting topic [$TOPIC]"
+	TOPIC=${TOPIC} $(dirname $0)/_kafka.delete-topic.sh
+	(( ++i ))
+done
+echo "Deleted $i topics"
--- _kafka.list-topics.sh
+++ _kafka.list-topics.sh
@@ -9,7 +9,6 @@ source "$(dirname $0)/.config.sh"
 
 : ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
 
-docker exec -it $(kafka_exec_cache) kafka-topics --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" \
+docker exec $(kafka_exec_cache) kafka-topics --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" \
 	--list 2>&1 \
-		| sed 's/\r//g' \
-			| grep -P ${KAFKA_TOPICS_FILTER-.}
+		| grep -P ${KAFKA_TOPICS_FILTER-.}
