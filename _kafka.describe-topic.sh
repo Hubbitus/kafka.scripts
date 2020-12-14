e2637496e0ad6ba99a68332c53393ce8756161b7
Pavel Alexeev <pahan@hubbitus.info>
2020-12-15 01:37:04 +0300

    Add _kafka.describe-topic.sh

---
+++ _kafka.describe-topic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to describe topic kafka topic (partitions, configs, policy and so on).
+# F.e.:
+# TOPICS=scoring ./_kafka.describe-topic.sh
+
+
+
+source "$(dirname $0)/.config.sh"
+
+: ${TOPIC?"Not enough vars set: TOPIC required.E.g.: 'TOPIC=uat-scoring.match-data.Scoring ./$(basename $0)'"}
+: ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
+
+docker exec $(kafka_exec_cache) kafka-topics --bootstrap-server "${KAFKA_BOOTSTRAP_SERVERS}" \
+	--describe --topic ${TOPIC} 2>&1
