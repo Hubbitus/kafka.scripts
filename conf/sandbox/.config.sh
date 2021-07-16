26a51fe3102722f7e712069edc7aa0270dd36bcc
Pavel Alexeev <Pavel_Alexeev@epam.com>
2021-07-16 15:00:19 +0300

    On sandbox use balancer URL

--- conf/sandbox/.config.sh
+++ conf/sandbox/.config.sh
@@ -6,7 +6,8 @@
 
 set -ueo pipefail
 
-: ${KAFKA_BOOTSTRAP_SERVERS:=PLAINTEXT://ecsc00a060af.epam.com:9092,PLAINTEXT://ecsc00a060b0.epam.com:9092,PLAINTEXT://ecsc00a060b1.epam.com:9092,PLAINTEXT://ecsc00a060b2.epam.com:9092,PLAINTEXT://ecsc00a060b3.epam.com:9092}
+#: ${KAFKA_BOOTSTRAP_SERVERS:=PLAINTEXT://ecsc00a060af.epam.com:9092,PLAINTEXT://ecsc00a060b0.epam.com:9092,PLAINTEXT://ecsc00a060b1.epam.com:9092,PLAINTEXT://ecsc00a060b2.epam.com:9092,PLAINTEXT://ecsc00a060b3.epam.com:9092}
+: ${KAFKA_BOOTSTRAP_SERVERS:=PLAINTEXT://kafka-sbox.epm-eco.projects.epam.com:9092}
 
 : ${SCHEMA_REGISTRY:=schema-registry-sbox.epm-eco.projects.epam.com:8081}
 
@@ -20,7 +21,7 @@ _conf_dir=$(dirname $(realpath "$BASH_SOURCE"))
 CONTAINER_CACHE_EXTRA_OPTIONS=()
 # In command below we mount /conf for holds certificates and keystores. Paswd file allso must contain password for kerberos account,
 # provided in sasl.kerberos.kinit.cmd line. Please be careful and NEVER commit sensitive information into git!!!
-KAFKACAT_SECURE_OPTIONS=( )
+KAFKACAT_SECURE_OPTIONS=()
 
 
 
