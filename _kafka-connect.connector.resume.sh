24b22b9bc6d14bad4ee4ca43bfa88cd7444a89a5
Pavel Alexeev <plalexeev@gid.ru>
2023-08-03 18:12:16 +0300

    Add scripts _kafka-connect.connector.pause.sh _kafka-connect.connector.resume.sh

---
+++ _kafka-connect.connector.resume.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Script to resume pauses kafka-connect connector.
+# API detailed description: https://docs.confluent.io/platform/current/connect/references/restapi.html#connectors
+
+source "$(dirname $0)/.shared.sh"
+
+: ${KAFKA_CONNECT_HOST?"Not enough vars set: KAFKA_CONNECT_HOST required"}
+: ${CONNECTOR?"Please provide CONNECTOR variable"}
+
+curl -X POST ${KAFKA_CONNECT_HOST}/connectors/${CONNECTOR}/pause
