cf7cd73ab079eeceb87a5d51964d332ccdddbe38
Pavel Alexeev <plalexeev@gid.ru>
2023-08-03 18:02:43 +0300

    _kafkacat.list-topics.sh - return topics list itself, without parent object

--- _kafkacat.list-topics.sh
+++ _kafkacat.list-topics.sh
@@ -7,4 +7,4 @@ source "$(dirname $0)/.shared.sh"
 
 : ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
 
-source "$(dirname $0)/_kafkacat.sh" -J -L "$@" | jq .
+source "$(dirname $0)/_kafkacat.sh" -J -L "$@" | jq '.topics[]'
