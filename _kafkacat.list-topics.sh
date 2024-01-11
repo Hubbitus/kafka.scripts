1e1971e49a213344de3b53802a3e7fbfb10c6d14
Pavel Alexeev <plalexeev@gid.ru>
2024-01-11 21:04:30 +0300

    Filter out useless 'SaslAuthenticateRequest failed' error

--- _kafkacat.list-topics.sh
+++ _kafkacat.list-topics.sh
@@ -8,4 +8,5 @@ source "$(dirname $0)/.shared.sh"
 
 : ${KAFKA_BOOTSTRAP_SERVERS?"Not enough vars set: KAFKA_BOOTSTRAP_SERVERS required"}
 
-source "$(dirname $0)/_kafkacat.sh" -J -L "$@" | jq '.topics[]'
+# SASL error filter is just workaround of https://github.com/confluentinc/confluent-kafka-dotnet/issues/1422 - there always error produced, but all works as expected
+source "$(dirname $0)/_kafkacat.sh" -J -L "$@" 2> >(grep -Fv 'SASL authentication error: SaslAuthenticateRequest failed: Local: Broker handle destroyed' >/dev/stderr) | jq '.topics[]'
